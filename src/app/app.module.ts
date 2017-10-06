a1494cc3e8d8b648f300103f05a0c3e352dcf555
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-10-06 13:41:42 -0500

    creando componente dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
