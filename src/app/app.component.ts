5fb9e46f0a79acb2c033737b92f311529aa90a40
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-10-06 13:35:50 -0500

    Agregando addFunction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,29 @@ import { Component } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
+export interface Todo {
+  
+    completed: boolean;
+    text: string;
+  }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  todoCollectionRef: AngularFirestoreCollection<any>;
-
+  todoCollectionRef: AngularFirestoreCollection<Todo>;
+  todo$: Observable<Todo[]>;
+  
   constructor(private db: AngularFirestore) {
     this.todoCollectionRef = this.db.collection<any>('todos');
+    this.todo$ = this.todoCollectionRef.valueChanges();
+  }
+
+  addTodo(todoDesc: string) {
+    if (todoDesc && todoDesc.trim().length) {
+      this.todoCollectionRef.add({ text: todoDesc, completed: false });
+    }
   }
 }
